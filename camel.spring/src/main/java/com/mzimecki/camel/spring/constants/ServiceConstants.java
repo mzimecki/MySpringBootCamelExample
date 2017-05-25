bb6d7f9d5bb2079ba2aba14d3872b55fbb421110
Marcin Zimecki <marcin.zimecki@lhsystems.com>
2017-05-25 09:31:32 +0200

    pom updated

--- camel.spring/src/main/java/com/mzimecki/camel/spring/constants/ServiceConstants.java
+++ camel.spring/src/main/java/com/mzimecki/camel/spring/constants/ServiceConstants.java
@@ -4,6 +4,7 @@ public final class ServiceConstants {
 	
 	public static final String HELLO_SERVICE_ENDPOINT = "direct:hello";
 	public static final String HELLO_ROUTE_ID = "HelloRoute";
+	public static final String QUEUE_READER_ROUTE_ID = "QueueReaderRoute";
 	
 	private ServiceConstants() {
 		
