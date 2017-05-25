112c29ed2e8c0bc655644479c1991bdaf20a8e60
Marcin Zimecki <marcin.zimecki@lhsystems.com>
2017-05-25 11:05:29 +0200

    unit tests added

--- camel.spring/src/main/java/com/mzimecki/camel/spring/processor/RedeliveryProcessor.java
+++ camel.spring/src/main/java/com/mzimecki/camel/spring/processor/RedeliveryProcessor.java
@@ -3,17 +3,18 @@ package com.mzimecki.camel.spring.processor;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import com.mzimecki.camel.spring.constants.ServiceConstants;
+
 public class RedeliveryProcessor implements Processor {
 	
-	private static final String REDELIVERY_COUNT_HEADER_NAME = "redeliveryCount";
 	private static final int MAX_REDELIVERIES = 5;
 	
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		final int redeliveryCount = exchange.getIn().getHeader(REDELIVERY_COUNT_HEADER_NAME, Integer.class).intValue();
+		final int redeliveryCount = exchange.getIn().getHeader(ServiceConstants.REDELIVERY_COUNT_HEADER_NAME, Integer.class).intValue();
 		
 		if (redeliveryCount <= MAX_REDELIVERIES) {
-			exchange.getIn().setHeader(REDELIVERY_COUNT_HEADER_NAME, redeliveryCount + 1);
+			exchange.getIn().setHeader(ServiceConstants.REDELIVERY_COUNT_HEADER_NAME, redeliveryCount + 1);
 		} else {
 			exchange.setProperty(Exchange.ROUTE_STOP, Boolean.TRUE);
 		}
--- camel.spring/src/test/java/com/mzimecki/camel/spring/route/QueueReaderRouteTest.java
+++ camel.spring/src/test/java/com/mzimecki/camel/spring/route/QueueReaderRouteTest.java
@@ -4,7 +4,9 @@ import javax.jms.ConnectionFactory;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.pool.PooledConnectionFactory;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.ExchangeBuilder;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -18,6 +20,8 @@ import org.springframework.context.support.AbstractApplicationContext;
 import com.mzimecki.camel.spring.constants.ServiceConstants;
 
 public class QueueReaderRouteTest extends CamelSpringTestSupport {
+
+	private static final String TEST_BODY_PAYLOAD = "sent to jms endpoint";
 
 	@Override
 	protected AbstractApplicationContext createApplicationContext() {
@@ -44,23 +48,50 @@ public class QueueReaderRouteTest extends CamelSpringTestSupport {
 		}
 	}
 	
+	private Exchange createExchange(final int redeliveryCount) {
+		final Exchange exchange = ExchangeBuilder.anExchange(context()).build();
+		exchange.getIn().setHeader(ServiceConstants.REDELIVERY_COUNT_HEADER_NAME, redeliveryCount);
+		return exchange;
+	}
+	
 	private void configureMockEndpoint() throws Exception {
-		final AdviceWithRouteBuilder mockSolr = new AdviceWithRouteBuilder() {
+		final AdviceWithRouteBuilder mockAdvice = new AdviceWithRouteBuilder() {
 
 			@Override
 			public void configure() throws Exception {
 				 replaceFromWith("direct:deadLetterQueueMock");
-				 interceptSendToEndpoint("jms:*").skipSendToOriginalEndpoint().process(exchange -> 
-				 	exchange.getIn().setBody("sent to jms endpoint"));
+				 interceptSendToEndpoint("jms:*")
+				 	.skipSendToOriginalEndpoint()
+				 	.process(exchange -> exchange.getIn().setBody(TEST_BODY_PAYLOAD));
 			};
 		};
 		
-		context().getRouteDefinition(ServiceConstants.QUEUE_READER_ROUTE_ID).adviceWith(context(), mockSolr);
+		context().getRouteDefinition(ServiceConstants.QUEUE_READER_ROUTE_ID).adviceWith(context(), mockAdvice);
 	}
 	
 	@Test
 	public void should_queue_reader_route_be_running() {
 		assertTrue(context().getRouteStatus(ServiceConstants.QUEUE_READER_ROUTE_ID).isStarted());
+	}
+	
+	@Test
+	public void should_increment_redelivery_count_and_send_to_jms_endpoint() throws Exception {
+		configureMockEndpoint();
+		final Exchange requestExchange = createExchange(0);
+		final Exchange resultExchange = template.send("direct:deadLetterQueueMock", requestExchange);
+		final int redeliveryCountResult = resultExchange.getIn().getHeader(ServiceConstants.REDELIVERY_COUNT_HEADER_NAME, Integer.class).intValue();
+		assertEquals(1, redeliveryCountResult);
+		assertEquals(TEST_BODY_PAYLOAD, resultExchange.getIn().getBody(String.class));
+	}
+	
+	@Test
+	public void should_not_increment_redelivery_count_and_not_reach_jms_enpoint() throws Exception {
+		configureMockEndpoint();
+		final Exchange requestExchange = createExchange(6);
+		final Exchange resultExchange = template.send("direct:deadLetterQueueMock", requestExchange);
+		final int redeliveryCountResult = resultExchange.getIn().getHeader(ServiceConstants.REDELIVERY_COUNT_HEADER_NAME, Integer.class).intValue();
+		assertEquals(6, redeliveryCountResult);
+		assertNull(resultExchange.getIn().getBody(String.class));
 	}
 
 }
